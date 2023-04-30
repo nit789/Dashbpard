d066110c4b13378c2570c426547407e91245068a
Nitin Kumar Singh <106854866+nitin0557@users.noreply.github.com>
2023-04-30 16:18:13 +0530

    main container fixed

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -4,6 +4,7 @@ import Sidebar from "./Sidebar";
 import Icons from "./Icons";
 
 const MainContainer = ({loading}) => {
+  console.log("ssss")
   return (
     <>
      <div className="home">Report  </div>
